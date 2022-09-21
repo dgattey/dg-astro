081f0a34977c05afe66e4e33b5902c0a0e39e937
Dylan Gattey <dylan.gattey@gmail.com>
2022-09-21 02:19:42 -0700

    feat: add ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-strict"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
