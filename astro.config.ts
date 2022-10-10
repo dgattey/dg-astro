73832fa3641cd99fc0f5581aa32c75f1f7cb596a
Dylan Gattey <dylan.gattey@gmail.com>
2022-10-10 16:56:01 -0700

    fix: disable cloudflare SSR, update packages, add more integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import cloudflare from "@astrojs/cloudflare";
-import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
-
+import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
+import compress from "astro-compress";
+
+const SITE = "https://dylangattey.com";
+
+// Listing of all pages in `/pages` for sitemap generation
+const PAGES = ["/"];
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), image(), partytown({}), prefetch(), sitemap()],
-  output: "server",
-  adapter: cloudflare(),
+  site: SITE,
+  integrations: [
+    preact(),
+    partytown({}),
+    astroImageTools,
+    prefetch(),
+    robotsTxt(),
+    sitemap({ customPages: PAGES.map((p) => `${SITE}${p}`) }),
+    compress(),
+  ],
 });
