033034f5c8f22c6c4bf839f7130efbd59399401e
Dylan Gattey <dylan.gattey@gmail.com>
2022-10-10 17:02:37 -0700

    fix: remove imagetools entirely

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
 
 const SITE = "https://dylangattey.com";
@@ -18,7 +17,6 @@ export default defineConfig({
   integrations: [
     preact(),
     partytown({}),
-    astroImageTools,
     prefetch(),
     robotsTxt(),
     sitemap({ customPages: PAGES.map((p) => `${SITE}${p}`) }),
